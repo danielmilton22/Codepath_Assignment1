82d2affec538f7d188f91a4ee1ce4df68c9e7614
danielmilton22 <dlmilton@ucsd.edu>
2022-02-08 22:53:59 -0800

    Assignment 2 Push

---
+++ Flix_prototype/Flix_prototype/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix_prototype
+//
+//  Created by Daniel Milton on 2/8/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
